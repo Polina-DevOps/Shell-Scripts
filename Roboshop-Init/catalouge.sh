ff5bb2c7fecb63b85318b53087a8637992a09803
Nageswar Polina <polinadevops2021@gmail.com>
2021-07-21 00:39:03 +0800

    updated npm command V1

--- Roboshop-Init/catalouge.sh
+++ Roboshop-Init/catalouge.sh
@@ -19,10 +19,10 @@ id roboshop >/dev/null
 if [ $? = 0 ]; then
 	sudo su - roboshop -c 'curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"'
 	cd /home/roboshop
-	sudo su - roboshop -c 'unzip -o /tmp/catalogue.zip'
+	sudo su - roboshop -c 'unzip -o /tmp/catalogue.zip'  >/dev/null
 	sudo su - roboshop -c 'mv catalogue-main catalogue'
 	cd /home/roboshop/catalogue
-	sudo su - roboshop -c 'npm install /home/roboshop/catalogue'
+	sudo su - roboshop -c 'npm install /home/roboshop/catalogue'  >/dev/null
 fi
 
 echo "Adding Mongo DB URL to catalogue configuration file"
@@ -30,7 +30,8 @@ echo "Adding Mongo DB URL to catalogue configuration file"
 sudo sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/g' /home/roboshop/catalogue/systemd.service
 if [ $? = 0 ]; then
 	sudo mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-	sudo systemctl daemon-reload
-	sudo systemctl start catalogue
-	sudo systemctl enable catalogue
+	sudo systemctl daemon-reload >/dev/null
+	sudo systemctl start catalogue >/dev/null
+	sudo systemctl enable catalogue >/dev/null
+	sudo systemctl status catalogue >/dev/null
 fi
