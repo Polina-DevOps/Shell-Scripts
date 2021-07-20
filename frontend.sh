2c45bc4c64ac8a98aed841274eb7c218f288aa8e
Nageswar Polina <polinadevops2021@gmail.com>
2021-07-20 19:48:38 +0800

    This is original script for frontend webserver deployment

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 ## Script to deploy frontend nginx server.
 
+#Changing hostname
+sudo hostnamectl set-hostname frontend
+sudo hostname
+
 echo "Install nginx server using yum utility"
-yum install nginx -y 1>/tmp/nginxinstallsuccess.out 2>/tmp/nginxinstallfail.out
-echo $?
+sudo yum install nginx -y 1>/tmp/nginxinstallsuccess.out 2>/tmp/nginxinstallfail.out
 if [ $? = 0 ]; then
   echo "Enabling nginx default startup"
-  systemctl enable nginx
+  sudo systemctl enable nginx
   echo "Starting nginx default startup"
-  systemctl start nginx
-  systemctl status nginx
+  sudo systemctl start nginx >/dev/null
+  sudo systemctl status nginx >/dev/null
 fi
 
 Download the HTDOCS content
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-echo $?
 if [ $? = 0 ]; then
   echo "Removing default nginx web page content"
-  cd /usr/share/nginx/html && rm -rf * && unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . && rm -rf frontend-master static && mv localhost.conf /etc/nginx/default.d/roboshop.conf
+  cd /usr/share/nginx/html && sudo rm -rf * && sudo unzip /tmp/frontend.zip >/dev/null && sudo mv frontend-main/* . && sudo mv static/* . && sudo rm -rf frontend-master static && sudo mv localhost.conf /etc/nginx/default.d/roboshop.conf
   echo "Restarting nginx web server"
-  systemctl restart nginx
+  sudo systemctl restart nginx
 fi
 
