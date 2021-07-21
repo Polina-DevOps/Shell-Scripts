f3b5567bc4f3f03c77ff21e0a0c4aff618b975e0
Nageswar Polina <polinadevops2021@gmail.com>
2021-07-21 20:33:23 +0800

    user v1

---
+++ Roboshop-Init/user.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+##Script to deploy user server
+
+echo "Installing nodejfs and GCC compiler"
+
+yum install nodejs make gcc-c++ -y >/dev/null
+if [ $? = 0 ]; then
+   id roboshop >/dev/null
+   if [ $? != 0 ]; then
+		echo "roboshop user creation"
+		 useradd roboshop
+		 echo "roboshop:roboshop" | chpasswd
+	fi
+fi
+
+echo "Downloading the installing nodejs depnendencies"
+
+id roboshop >/dev/null
+if [ $? = 0 ]; then
+	curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+	if [ $? = 0 ]; then
+		cd /home/roboshop && unzip -o /tmp/user.zip && mv user-main user && cd /home/roboshop/user && npm install --unsafe-perm=true -g now && chown -R roboshop:roboshop /home/roboshop
+	fi
+fi
+
+echo "Adding Mongo DB URL to user configuration file"
+
+if [ $? = 0 ]; then
+then
+	 mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+	 systemctl daemon-reload
+	 systemctl start user
+	 systemctl enable user
+	 systemctl status user
+fi
