becb918f96000d674420064b9481dcdbe435cf10
Nageswar Polina <polinadevops2021@gmail.com>
2021-07-22 21:40:44 +0800

    updated sed v1.1

--- Roboshop-Init/frontend.sh
+++ Roboshop-Init/frontend.sh
@@ -20,6 +20,7 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/fron
 if [ $? = 0 ]; then
   echo "Removing default nginx web page content"
   cd /usr/share/nginx/html && sudo rm -rf * && sudo unzip /tmp/frontend.zip >/dev/null && sudo mv frontend-main/* . && sudo mv static/* . && sudo rm -rf frontend-master static && sudo mv localhost.conf /etc/nginx/default.d/roboshop.conf
+  sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/'  -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
   echo "Restarting nginx web server"
   sudo systemctl restart nginx
 fi
