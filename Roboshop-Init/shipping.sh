4d32eb148af621e27877a2d4cdd6c5e3b858dd39
Nageswar Polina <polinadevops2021@gmail.com>
2021-07-23 19:47:50 +0800

    V1

--- Roboshop-Init/shipping.sh
+++ Roboshop-Init/shipping.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 ##Script to deploy Shipping server
+[ ! -d /var/tmp ] && mkdir /var/tmp
+chmod 755 /var/tmp
+[ ! -f /var/tmp/roboshop.log ] && touch /var/tmp/roboshop.log
+chmod 755 /var/tmp/roboshop.log
 
-echo "Install Maven, This will install Java too"
+LOG=/var/tmp/roboshop.log
 
-yum install maven -y >/dev/null
+echo "Install Maven & Java"
+
+yum install maven -y >>$LOG
 if [ $? = 0 ]; then
    id roboshop >/dev/null
    if [ $? != 0 ]; then
 		  echo "roboshop user creation"
-		  useradd roboshop
+		  useradd roboshop >>$LOG
 	fi
 fi
 
@@ -17,9 +23,9 @@ echo "Downloading the Repo"
 id roboshop >/dev/null
 if [ $? = 0 ]; then
 	cd /home/roboshop
-	curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
+	curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip" &>>$LOG
 	if [ $? = 0 ]; then
-		cd /home/roboshop && rm -rf shipping-main shipping && unzip -o /tmp/shipping.zip >/dev/null && rm -rf shipping && mv shipping-main shipping && cd shipping && mvn clean package && mv target/shipping-1.0.jar shipping.jar && chown -R roboshop:roboshop /home/roboshop
+		cd /home/roboshop && rm -rf shipping && unzip -o /tmp/shipping.zip &>>$LOG && mv shipping-main shipping && cd shipping && mvn clean package && mv target/shipping-1.0.jar shipping.jar && chown -R roboshop:roboshop /home/roboshop
 	fi
 fi
 
